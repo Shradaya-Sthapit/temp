24faf65032b449feea04d0280216296358a0acac
Shradaya Sthapit <1shradaya@gmail.com>
2021-07-17 17:36:10 +0545

    first commit

---
+++ 1-intro.js
@@ -0,0 +1,6 @@
+//globals
+console.log(__dirname);
+
+setInterval(()=>{
+    console.log('HELLO WORLD')
+},1000)
---
+++ 2-modules.js
@@ -0,0 +1,12 @@
+//Modules
+
+const names= require('./3-names')
+const sayHi= require('./4-utils')
+const data= require('./5- alternative')
+require('./6-add')
+// console.log(data)
+// console.log(names)
+
+//  sayHi('susan')
+//  sayHi(names.john)
+
